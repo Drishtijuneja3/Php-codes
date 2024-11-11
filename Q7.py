6946175c29a8d209c6d534378bbd924789d283f7
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2024-11-11 11:44:10 +0530

    Create Q7.py

---
+++ Q7.py
@@ -0,0 +1,7 @@
+my_string="Bharati"
+pos=int(input("Enter the position of character to print"))
+if 0<=pos<len(my_string):
+  print(f"The character at specified string:",{my_string[pos]})
+else:
+  print("Invalid position")
+print("This code is written by Drishti")
