643555e4dbb0838549713bc9e15cba4f0f65b324
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2024-11-11 11:49:27 +0530

    Create Q9.py

---
+++ Q9.py
@@ -0,0 +1,7 @@
+my_string="Bharati"
+reversed_string= " "
+for char in my_string:
+  reversed_string=char+reversed_string
+print("Original string:", my_string)
+print("Reversed string:", reversed_string)
+print("This code is written by Drishti")
