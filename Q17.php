b8a6c37e21c6d4931d0c1428cd94138695ee9ef7
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-17 22:51:51 +0530

    Update Q17.php

--- Q17.php
+++ Q17.php
@@ -4,7 +4,7 @@ $y=array("u"=>"Yellow", "v"=>"Orange", "w"=>"Pink");
 $z=$x+$y;
 var_dump($z);
 var_dump($x==$y);
-var_dump(x===$y);
+var_dump($x===$y);
 var_dump($x!=$y);
 var_dump($x<>$y);
 var_dump($x!==$y);
