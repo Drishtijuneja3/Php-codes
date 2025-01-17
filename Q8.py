8a58b64273affeccebc436291ea9f985e1b3529b
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-17 22:18:37 +0530

    Update and rename Q8.py to Q8.php

--- Q8.py
+++
@@ -1,8 +0,0 @@
-my_string="Bharati"
-char_to_find=input("Enter character you want to find: ")
-positions=[i for i, char in enumerate(my_string) if char==char_to_find]
-if positions:
-  print(f"The character {char_to_find} is found at positions {positions}")
-else:
-  print(f"The character "{char_to_find}" is not found in string")
-print("This code is written by Drishti")
