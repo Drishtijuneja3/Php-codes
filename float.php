e44f0baed7dcfc70f8336e333cbcd2f011f7284e
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-25 17:06:53 +0530

    Update and rename Q4.php to float.php

---
+++ float.php
@@ -0,0 +1,9 @@
+<?php
+//code to demonstrate floating point numbers
+    $a = 1.234;
+    var_dump($a);
+    
+    $b = 10.2e3;
+    var_dump($b);
+    echo "This program is written by Drishti";
+?>
