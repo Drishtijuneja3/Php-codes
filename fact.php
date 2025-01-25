b6f9f1004554997e1d1447fbde136b0a878a42a5
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 04:07:45 +0530

    Create fact.php

---
+++ fact.php
@@ -0,0 +1,9 @@
+<?php
+//code to print factorial of a number
+$n=4;
+$fact=1;
+for($x=$n;$x>=1;$x--){
+  $fact=$fact*$x;}
+echo "Factorial of $n is $fact";
+echo "This program is written by Drishti";
+?>
