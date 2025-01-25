3da7b040f0243f98a87ac79034006d3a29ff168a
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 03:19:26 +0530

    Update and rename Q14.php to logical.php

--- logical.php
+++ logical.php
@@ -1,5 +1,5 @@
 <?php
-
+//code to demonstrate logical operators or to check whether an year is leap year or not
     $year = 2014;
     
     // Leap years are divisible by 400 or by 4 but not 100
@@ -11,5 +11,5 @@
     {
         echo "$year is not a leap year.";
     }
-
+echo "This program is written by Drishti";
 ?>
