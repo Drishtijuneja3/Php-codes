353f158f9892601e25f9e753ffda9d3851172127
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-25 17:08:38 +0530

    Update and rename Q5.php to boolean.php

--- boolean.php
+++ boolean.php
@@ -1,7 +1,7 @@
 <?php
-
+//code to demonstrate boolean data type
     // Assign the value TRUE to a variable
     $show_error = true;
     var_dump($show_error);
-
+    echo "This program is written by Drishti";
 ?>
