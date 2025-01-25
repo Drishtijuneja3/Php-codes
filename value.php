fcdb9bba7904db40b732243b0df67cd2b07030cf
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 04:35:01 +0530

    Create value.php

---
+++ value.php
@@ -0,0 +1,8 @@
+<?php
+//code to demosntrate passing by value in function
+function addFunction($num1,$num2){
+  $sum=$num1+$num2;
+  echo "Sum of the two numbers is: $sum";}
+addFunction(10,20);
+echo "This program is written by Drishti";
+?>
