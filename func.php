62b82c39c56244297d74598133ccf707f8daa061
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 04:30:55 +0530

    Create func.php

---
+++ func.php
@@ -0,0 +1,7 @@
+<?php
+//code to demonstrate user defined function
+function writeMessage(){
+  echo "You are a really nice person, Have a nice time!";}
+writeMessage();
+echo "This program is written by Drishti";
+?>
