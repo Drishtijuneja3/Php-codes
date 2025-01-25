e270825c04c07f4598f3fe8b24d38fdd81ffe7df
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-25 17:32:10 +0530

    Update and rename Q8.php to null.php

--- null.php
+++ null.php
@@ -1,10 +1,10 @@
 <?php
-
+// code to demonstrate null value
     $a = NULL;
     var_dump($a);
      
     $b = "Hello World!";
     $b = NULL;
     var_dump($b);
-
+    echo "This code is written by Drishti";
 ?>
