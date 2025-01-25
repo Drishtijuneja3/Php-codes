be0d0d9b0af0c045c6d58fbe9202eb5420e98906
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 04:38:06 +0530

    Update resource.php

--- resource.php
+++ resource.php
@@ -3,5 +3,5 @@
     // Open a file for reading
     $handle = fopen("note.txt", "r");
     var_dump($handle);
- echo "This code is written by Drishti";
+ echo "This program is written by Drishti";
 ?>
