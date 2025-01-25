a8b5b52f0379e6fe6012f87bbeba79eeb424a6a8
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 03:39:59 +0530

    Update and rename Q18.php to if.php

---
+++ if.php
@@ -0,0 +1,10 @@
+<?php
+//code to demonstrate simple if statement
+    $a = 10;
+    $b = 20;
+    if($a < $b)
+    {
+    	echo "Out of $a and $b, $a is smaller" ;
+    }
+    echo "This code is written by Drishti";
+?>
