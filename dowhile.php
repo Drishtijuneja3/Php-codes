8ee33720dd0b4995c2fa9186acdbcfb1d5daa0a4
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-01-26 03:56:14 +0530

    Update and rename Q24.php to dowhile.php

---
+++ dowhile.php
@@ -0,0 +1,9 @@
+<?php
+//code to demonstrate do-while loop
+    $i = 1;
+    do{
+        echo "Execution Number: $i <br>";
+    	$i++;
+    }while($i >200&&$i<400);
+    echo "This program is written by Drishti";
+?>
