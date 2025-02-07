13b7d57370aa5eff2df6ab070a51321a6a30462d
Drishtijuneja3 <154918119+Drishtijuneja3@users.noreply.github.com>
2025-02-07 12:01:49 +0530

    Create numarray.php

---
+++ numarray.php
@@ -0,0 +1,14 @@
+<?php
+$numbers=array(1,2,3,4,5);
+for each($numbers as $value)
+  { echo "Value is $value <br>";
+  }
+$numbers[0]="one";
+$numbers[1]="two";
+$numbers[2]="three";
+$numbers[3]="four";
+$numbers[4]="five";
+for each($numbers as $value)
+  { echo "Value is $value <br>"; }
+echo "This program is written by Drishti";
+?>
